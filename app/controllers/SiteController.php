67c9c893d6cb73c19f23eaa43e68c89dbddd3123
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-12-19 22:17:48 +0330

    add request filter & validation

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -3,6 +3,7 @@
 namespace App\app\controllers;
 
 use App\core\BaseController;
+use App\core\Request;
 
 class SiteController extends BaseController
 {
@@ -15,8 +16,16 @@ class SiteController extends BaseController
         return $this->render('home', $param);
     }
 
-    public function HandleContactForm()
+    public function ShowContactForm(Request $request)
     {
+        return $this->render('contact', ['request' => $request]);
+    }
 
+    public function HandleContactForm(Request $request): bool|array|string
+    {
+        $request->loadData($request->getBody());
+        $request->validate();
+
+        return $this->render('contact', ['request' => $request]);
     }
 }
